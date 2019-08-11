0e218e47e1c40779b02cbb0a1f093c69c53b27a9
Irving-Sosa-Dev <irvingmtx11@gmail.com>
2019-08-11 12:52:54 -0600

    Se cambia titulo a libreria

---
+++ footer.php
@@ -0,0 +1,12 @@
+<div class="navbar navbar-default navbar-fixed-bottom">
+    <div class="container">
+      <p class="navbar-text pull-left">&copy <?php echo date('Y');?> La Libreria
+           <a href="" target="_blank" style="color: #ecf0f1">SEMINARIO</a>
+      </p>
+   </div>
+</div>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="js/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <!-- Latest compiled and minified JavaScript -->
+	<script src="js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
